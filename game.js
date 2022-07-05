2d47694bd08e125d902130ce7e2019870f9f7e32
Romila Rangel <85246670+romilagui@users.noreply.github.com>
2022-07-05 21:43:39 +0100

    Add files via upload

---
+++ game.js
@@ -0,0 +1,9 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+const randomChosenColour = buttonColours[randomNumber];
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+ 
+}
+gamePattern.push(randomChosenColour);
